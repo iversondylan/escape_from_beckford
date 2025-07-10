c8fe1ab7e907b2403231cbb3f88d1583221a5a7a
Dylan Iverson <iversondylan@gmail.com>
2025-07-10 13:52:40 -0700

    more on battle_rewards

--- escapefrombeckford/scenes/battle_rewards/battle_rewards.gd
+++ escapefrombeckford/scenes/battle_rewards/battle_rewards.gd
@@ -54,7 +54,44 @@ func _show_card_reward() -> void:
 	
 	var card_reward := CARD_REWARD.instantiate() as CardReward
 	add_child(card_reward)
-	#card_reward.card_reward_selected.connect(_on_card_reward_taken)
+	card_reward.card_reward_selected.connect(_on_card_reward_taken)
+	
+	var card_choices: Array[CardData] = []
+	var possible_cards: Array[CardData] = GameRecord.draftable_cards.cards.duplicate(true)
+	
+	for i in run_account.card_reward_choices:
+		_calculate_card_chances()
+		var roll := randf_range(0.0, card_reward_total_weight)
+		
+		for rarity: CardData.Rarity in card_rarity_weights:
+			if card_rarity_weights[rarity] > roll:
+				_modify_weights(rarity)
+				var rolled_card := _get_random_possible_card(possible_cards, rarity)
+				card_choices.append(rolled_card)
+				possible_cards.erase(rolled_card)
+				break
+	
+	card_reward.card_choices = card_choices
+	card_rewards.show()
+
+func _calculate_card_chances() -> void:
+	card_reward_total_weight = run_account.common_weight + run_account.uncommon_weight + run_account.rare_weight
+	card_rarity_weights[CardData.Rarity.COMMON] = run_account.common_weight
+	card_rarity_weights[CardData.Rarity.UNCOMMON] = run_account.common_weight + run_account.uncommon_weight
+	card_rarity_weights[CardData.Rarity.RARE] = card_reward_total_weight
+
+func _modify_weights(rarity_rolled: CardData.Rarity) -> void:
+	if rarity_rolled == CardData.Rarity.RARE:
+		run_account.rare_weight = RunAccount.BASE_RARE_WEIGHT
+	else:
+		run_account.rare_weight = clampf(run_account.rare_weight + 0.3, run_account.BASE_RARE_WEIGHT, 5.0)
+
+func _get_random_possible_card(possible_cards: Array[CardData], rarity: CardData.Rarity) -> void:
+	var all_possible_cards := possible_cards.filter(
+		func(card: CardData):
+			return card.rarity == rarity
+	)
+	return all_possible_cards.pick_random()
 
 func _on_gold_reward_taken(n_gold: int) -> void:
 	if !run_account:
